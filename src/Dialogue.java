78a828fd885c0d45f1bde3cc37abbbbb12b0ce3d
gravo-lan <alvintsolh@gmail.com>
2023-10-01 13:00:16 +0800

    Completed Chapter 0

--- src/Dialogue.java
+++ src/Dialogue.java
@@ -1,2 +1,68 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
+import java.util.*;
+
 public class Dialogue {
+    public static ArrayList<String> dialogue = new ArrayList<>();
+    public static String flow = "0000100001";
+    public static void introduction() throws Exception {
+        Scanner input = new Scanner(System.in);
+        JSONParser parser = new JSONParser();
+        Main.loadData((JSONArray) parser.parse(new FileReader("./src/data.json")));
+        load();
+        runD();
+        System.out.println("[1] Examine the drawer\n[2] Grab something just in case\n[3] Nevermind, I'll by a new phone...");
+        int option = input.nextInt();
+        if (option==1) text("You grab onto a nearby stick as you feel yourself sucked into the abyss...");
+        else if (option==2) text("You grab a nearby stick on your desk, before you suddenly get sucked into the abyss...");
+        else System.exit(0);
+        Main.progress++;
+        Main.saveGame();
+        floor1();
+    }
+
+    public static void floor1() throws Exception {
+        Scanner input = new Scanner(System.in);
+        JSONParser parser = new JSONParser();
+        Main.loadData((JSONArray) parser.parse(new FileReader("./src/data.json")));
+        load();
+        runD();
+        if (Main.progress==9) {
+            System.out.println("Follow the guiding light?\n[1] Yes, I'd rather be able to see! (Exploration Tutorial)\n[2] No, maybe the laughter is coming from someone who could help me! (Combat Tutorial)\n(Don't worry, you can always do one or the other later!)");
+            int option = input.nextInt();
+        }
+    }
+    private static void text(String str) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        if (Main.hp<=0) {
+            System.out.println("Oh no! You have died!");
+        }
+        System.out.print(str);
+        Scanner stall = new Scanner(System.in);
+        stall.nextLine();
+    }
+
+    private static void load() throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject obj = (JSONObject) parser.parse(new FileReader("./src/dialogue.json"));
+        JSONArray arr = (JSONArray) obj.get("Dialogue");
+        for (Object o : arr) {
+            dialogue.add((String) o);
+        }
+    }
+
+    private static void runD() {
+        while (Integer.parseInt(String.valueOf(flow.charAt((int) Main.progress)))==0) {
+            text(dialogue.get((int) Main.progress));
+            Main.progress++;
+        }
+        try {
+            Main.saveGame();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,72 @@
-import org.json.simple.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
+
 import java.util.*;
 import java.io.*;
+import java.nio.file.*;
 public class Main {
+
+    public static String name, weapon;
+    public static long hp, xp, progress,level;
     public static void main(String[] args) throws Exception {
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         JSONParser parser=new JSONParser();
-        JSONArray data = (JSONArray) parser.parse(new FileReader("data.json"));
+        JSONArray data = (JSONArray) parser.parse(new FileReader("./src/data.json"));
+        loadData(data);
+        System.out.printf("Welcome! \n[1] Continue as %s \n[2] Start a New Game\n",name);
+        Scanner input = new Scanner(System.in);
+        if (input.nextInt()==2) newGame();
+        play();
+    }
+
+    private static void play() throws Exception {
+        if (progress<5) Dialogue.introduction();
+        else if (progress>5) Dialogue.floor1();
+    }
+
+    public static void loadData(JSONArray data) {
+        for (Object o : data) {
+            JSONObject obj = (JSONObject) o;
+            name = (String) obj.get("Name");
+            hp = (long) obj.get("HP");
+            weapon = (String) obj.get("Weapon");
+            xp = (long) obj.get("XP");
+            progress = (long) obj.get("Progress");
+            level = (long) obj.get("Level");
+        }
+    }
+    
+    public static void saveGame() throws Exception {
+        JSONParser parser=new JSONParser();
+        JSONArray data = (JSONArray) parser.parse(new FileReader("./src/data.json"));
+        for (Object o: data) {
+            JSONObject obj = (JSONObject) o;
+            obj.put("Name", name);
+            obj.put("HP", hp);
+            obj.put("Weapon",weapon);
+            obj.put("XP", xp);
+            obj.put("Progress",progress);
+            obj.put("Level", level);
+        }
+        write(data);
+    }
+
+    private static void newGame() throws Exception {
+        Files.copy(Paths.get("./src/defaultData.json"), Paths.get("./src/data.json"), StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("Enter your name: ");
+        Scanner name = new Scanner(System.in);
+        JSONParser parser=new JSONParser();
+        JSONArray data = (JSONArray) parser.parse(new FileReader("./src/data.json"));
+        for (Object o: data) {
+            JSONObject obj = (JSONObject) o;
+            obj.put("Name",name.next());
+        }
+        write(data);
+    }
+    
+    private static void write(JSONArray data) throws IOException {
+        FileWriter file = new FileWriter("./src/data.json");
+        file.write(data.toJSONString());
+        file.flush();
     }
 }
